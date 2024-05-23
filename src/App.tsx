2406791d52f945b5916cba11dde6ac88078fa9bd
Rudresh-sisode <rudranrajput@gmail.com>
2024-05-23 16:41:45 +0530

    designing | other business logic added

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,18 @@
 
 import { useState } from 'react';
 import './App.css';
-import Vapi from '@vapi-ai/web';
+import './assets/fonts/font.css';
 
-
+import MeeraAssistance from './Components/MeeraAssistance/MeeraAssistance';
 
 
 function App() {
   
-  const vapi = new Vapi(import.meta.env.VITE_REACT_APP_VAPI_PUBLIC_API as string); // Vapi public key
-  
-  const [isListening, setIsListening] = useState(false);
 
-  const startCall = () => {
-    vapi.start(import.meta.env.VITE_REACT_APP_VAPI_ASSISTANCE_ID as string); // assistance ID
-
-    setIsListening(true);
-  };
-  const stopCall = () => {
-    vapi.stop();
-    setIsListening(false);
-  };
-
-  vapi.on("error", (e) => {
-    console.error(e);
-    vapi.stop();
-    setIsListening(false);
-  });
 
   return (
     <div className='App'>
-      <button style={{marginRight:'10px'}} onClick={startCall}>start</button>
-      <button onClick={stopCall}>stop</button>
-      {isListening ? (
-        <div className='listening'>
-          Assistance is listening...
-        </div>
-      ) : (
-        <div className='not-listening'>
-          Assistance has stopped.
-        </div>
-      )}
+      <MeeraAssistance />
     </div>
   );
 }
